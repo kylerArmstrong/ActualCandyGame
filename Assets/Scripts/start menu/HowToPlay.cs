b97fb0ae9938338a80d4573edeb09c0571cc5047
kylerArmstrong <143137470+kylerArmstrong@users.noreply.github.com>
2025-12-02 11:06:47 -0600

    start menu

---
+++ Assets/Scripts/start menu/HowToPlay.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HowToPlay : MonoBehaviour
+{
+    public GameObject howToText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void click()
+    {
+        howToText.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/start menu/Play.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void click()
+    {
+        SceneManager.LoadScene("home");
+    }
+}
